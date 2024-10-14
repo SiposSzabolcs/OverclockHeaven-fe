053927406534b6190c24b6b528d6009dbb9bb4de
Sipos Szabolcs <siposszabolcs10@gmail.com>
2024-10-14 13:41:19 +0200

    Landing page complete

--- src/components/image-text/image-text.component.ts
+++ src/components/image-text/image-text.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-image-text',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './image-text.component.html',
-  styleUrl: './image-text.component.css'
+  styleUrl: './image-text.component.css',
 })
 export class ImageTextComponent {
-
+  @Input() Direction: string = 'left';
+  @Input() h1Content: string = '';
+  @Input() h2Content: string = '';
+  @Input() pContent: string = '';
+  @Input() buttonContent: string = '';
+  @Input() imgSrc: string = '';
 }
