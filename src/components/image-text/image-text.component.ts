b9a6bc9c9131187d328673a4114d233b0321c709
Sipos Szabolcs <siposszabolcs10@gmail.com>
2024-10-14 12:04:26 +0200

    Added image-text component

---
+++ src/components/image-text/image-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-text.component.html',
+  styleUrl: './image-text.component.css'
+})
+export class ImageTextComponent {
+
+}
