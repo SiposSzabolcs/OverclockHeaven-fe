d7e414232223e1697b20f085c90f1e350128d6ae
Sipos Szabolcs <siposszabolcs10@gmail.com>
2024-11-01 15:56:07 +0100

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,15 @@ import { customInterceptor } from '../interceptors/custom.interceptor';
 import { provideNgxStripe } from 'ngx-stripe';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { environment } from '../environments/environment';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([customInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideCharts(withDefaultRegisterables()),
     provideNgxStripe(environment.stripePublicKey),
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
   ],
 };
