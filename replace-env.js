504fe141adbd417422104bf6f7333944f45fd757
Sipos Szabolcs <siposszabolcs10@gmail.com>
2024-11-01 15:18:33 +0100

    .

--- replace-env.js
+++ replace-env.js
@@ -2,11 +2,13 @@ const fs = require("fs");
 
 // Get environment variables from the Render environment
 const stripePublicKey = process.env.STRIPE_API_KEY;
+const baseUrl = process.env.baseUrl;
 
 // Template for the environment.prod.ts file
 const envConfig = `
 export const environment = {
-  stripePublicKey: '${stripePublicKey}'
+  stripePublicKey: '${stripePublicKey}',
+  baseUrl: '${baseUrl}'
 };
 `;
 
